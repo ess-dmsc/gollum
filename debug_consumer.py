86e3f1defc9fceba1c30c8fda92439d09d9584ee
stefanosathanasopoul <stefanos.athanasopoulos@ess.eu>
2022-12-08 10:21:08 +0100

    adding requirements, to do list, debug consumer & changes in gollum writer

---
+++ debug_consumer.py
@@ -0,0 +1,18 @@
+from confluent_kafka import Consumer
+import uuid
+
+
+consumer_config = {
+    "bootstrap.servers": "127.0.0.1",
+    "group.id": uuid.uuid4(),
+    "default.topic.config": {"auto.offset.reset": "latest"},
+}
+consumer = Consumer(consumer_config)
+consumer.subscribe(["gollum"])
+
+while True:
+    msg = consumer.poll(timeout=0.5)
+    if msg is None:
+        continue
+    elif msg.value():
+        print(msg.value())
