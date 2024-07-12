f709b03bf4a14e2529468e83401c70c934f938b8
Josephine <josephine.hojding@ess.eu>
2024-07-12 09:52:05 +0200

    small fixes

--- main.py
+++ main.py
@@ -109,7 +109,9 @@ if __name__ == "__main__":
                     for body in bodies:
                         body_id = body["id"]
                         if last_published_ns[body_id] + PUBLISH_MS * 1000000 < timestamp_ns:
-                            if body_id not in last_positions or has_moved(last_positions[body_id], body["pos"], last_rotations[body_id], body["rot"], is_valid[body_id], body["valid"]):
+                            if body_id not in last_positions or has_moved(last_positions[body_id], body["pos"],
+                                                                          last_rotations[body_id], body["rot"],
+                                                                          is_valid[body_id], body["valid"]):
                                 last_published_ns[body_id] = timestamp_ns
                                 last_positions[body_id] = body["pos"]
                                 last_rotations[body_id] = body["rot"]
